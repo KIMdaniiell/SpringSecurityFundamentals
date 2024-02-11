499b380de90a116ea2ae83be6f21415590f9abde
Kim Daniel <kimdaniiell@gmail.com>
2024-02-11 22:48:27 +0300

    Implemented demo authorisation

---
+++ src/main/java/org/example/springsecurityfundamentals/controllers/DemoController.java
@@ -0,0 +1,13 @@
+package org.example.springsecurityfundamentals.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
+}
